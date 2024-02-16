94c7a76a5c63d0b500558910356a3d8156ef9045
Willian Ribeiro <57929088+WillianVMR@users.noreply.github.com>
2024-02-16 12:26:57 -0300

    Reestruturação do projeto para um modelo mais apropriado

---
+++ controler.py
@@ -0,0 +1,7 @@
+from packages.ibge_data_extracter_package.ibge_data_loader import DataLoaderComposition
+
+raw_data_reference = 'Data/Dados-IBGE'
+relational_database_reference = 'Database'
+
+ibge_composition_folder_path = 'Data/Dados-IBGE/COMPOSICAO-POPULACAO'
+ibge_pib_folder_path = 'Data/Dados-IBGE/PIB'
