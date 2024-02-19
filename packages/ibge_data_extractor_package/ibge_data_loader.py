46a3ef517d75ad19857c9df4394c91edb0d59a9d
Willian Ribeiro <57929088+WillianVMR@users.noreply.github.com>
2024-02-19 19:11:15 -0300

    Criador de relacionamento entre tabelas

--- packages/ibge_data_extractor_package/ibge_data_loader.py
+++ packages/ibge_data_extractor_package/ibge_data_loader.py
@@ -9,6 +9,8 @@ class DataLoaderPIB:
         self.file_paths = []
         self.dataset = pd.DataFrame()
         self.filtered_dataset = pd.DataFrame()
+        self.citie_dimension_table = pd.DataFrame()
+        self.pib_table = pd.DataFrame()
     
     def find_excel_files(self):
         for file_name in os.listdir(self.folder_path):
@@ -23,10 +25,37 @@ class DataLoaderPIB:
             self.dataset = pd.concat([self.dataset, temp_df], ignore_index=True)
             self.filtered_dataset = self.dataset.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7, 38, 39, 40, 41, 42]]
         self.filtered_dataset.columns = ['ano', 'codigo_grande_regiao', 'nome_grande_regiao', 'codigo_uf', 'sigla_uf', 'nome_uf', 'codigo_municipio', 'nome_municipio', 'pib_bruto', 'pip_per_capta', 'atividade_principal_contribuicao', 'atividade_secundaria_contribuicao', 'atividade_tercearia_contribuicao']
+        
+    def create_tables(self):
+        temp_cities_dimension_table = self.filtered_dataset[['ano', 'codigo_grande_regiao', 'nome_grande_regiao', 'codigo_uf', 'sigla_uf', 'nome_uf', 'codigo_municipio', 'nome_municipio']]
+        temp_cities_dimension_table = temp_cities_dimension_table[temp_cities_dimension_table['ano'] == 2021]
+        self.citie_dimension_table = temp_cities_dimension_table
+        temp_pib_table = self.filtered_dataset[['codigo_municipio', 'pib_bruto', 'pip_per_capta', 'atividade_principal_contribuicao', 'atividade_secundaria_contribuicao', 'atividade_tercearia_contribuicao']]
+        self.pib_table = temp_pib_table
     
     def save_to_sql(self):
         engine = create_engine(self.database_uri)
-        self.filtered_dataset.to_sql('dados_pib', con=engine, if_exists='replace', index=False)
+        
+        # Create dimensao_cidades table
+        self.citie_dimension_table.to_sql('dimensao_cidades', con=engine, if_exists='replace', index=False)
+        
+        # Create dados_pib table with foreign key constraint
+        create_pib_table_query = '''
+        CREATE TABLE IF NOT EXISTS dados_pib (
+            codigo_municipio INTEGER,
+            pib_bruto FLOAT,
+            pip_per_capta FLOAT,
+            atividade_principal_contribuicao TEXT,
+            atividade_secundaria_contribuicao TEXT,
+            atividade_tercearia_contribuicao TEXT,
+            FOREIGN KEY (codigo_municipio) REFERENCES dimensao_cidades(codigo_municipio)
+        );
+        '''
+        with engine.connect() as conn:
+            conn.execute(create_pib_table_query)
+        
+        # Insert data into dados_pib table
+        self.pib_table.to_sql('dados_pib', con=engine, if_exists='replace', index=False)
     
     def process(self):
         self.find_excel_files()
