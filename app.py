435fd2c2730dc6f3e4d1635e3d4473b899bb0285
Willian Ribeiro <57929088+WillianVMR@users.noreply.github.com>
2024-02-19 16:17:58 -0300

    Aplicativo configurado e funcional

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import subprocess
 
 app = Flask(__name__)
@@ -9,13 +9,31 @@ def index():
 
 @app.route('/run_etl_script', methods=['POST'])
 def run_etl_script():
-    # Run your ETL script
-    try:
-        output = subprocess.check_output(['python', 'your_etl_script.py'], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        output = e.output
-    
-    return output
+    # Identifica qual script rodar baseado no JSON recebido
+    data = request.get_json()
+    script_name = data.get('scriptName')
+
+    # Dicionário para mapear nomes de script para arquivos Python correspondentes
+    scripts = {
+        'ETL 1': 'controler_ibge.py',
+        'ETL 2': 'controler_sympla_pandas.py',
+        'ETL 3': 'controler_sympla_spark.py',
+    }
+
+    # Seleciona o arquivo de script com base na entrada do usuário
+    script_file = scripts.get(script_name)
+
+    if script_file:
+        try:
+            # Executa o script selecionado
+            output = subprocess.check_output(['python', script_file], stderr=subprocess.STDOUT, text=True)
+            return jsonify({'message': output})
+        except subprocess.CalledProcessError as e:
+            # Retorna a saída de erro se o script falhar
+            return jsonify({'error': e.output}), 400
+    else:
+        # Retorna um erro se o nome do script não for reconhecido
+        return jsonify({'error': 'Script name not recognized'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
